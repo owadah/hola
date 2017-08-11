ec8cac75031c1b42782b8b498d3a8ce99d7e62ac
Ondra Chaloupka <ochaloup@redhat.com>
2017-08-11 17:56:01 +0200

    new version

--- fix-dependencies.sh
+++ fix-dependencies.sh
@@ -4,7 +4,14 @@ set -x -e
 rm -rf target/hola-swarm.jar.dir
 unzip -q -d target/hola-swarm.jar.dir target/hola-swarm.jar
 VERSION=`find target/hola-swarm.jar.dir -name 'httpclient*jar' | head -n 1 | sed 's|.*-\(.*\)\.jar$|\1|'`
-echo "- org.apache.httpcomponents:httpclient:${VERSION}" >> target/hola-swarm.jar.dir/META-INF/wildfly-swarm-manifest.yaml
+
+if [ "x$1" = "xnew" ]; then
+  TOADD="org.apache.httpcomponents:httpclient:jar:${VERSION}: null"
+else
+  TOADD="- org.apache.httpcomponents:httpclient:jar:${VERSION}"
+fi
+echo "$TOADD" >> target/hola-swarm.jar.dir/META-INF/wildfly-swarm-manifest.yaml
+
 [ ! -f hola-swarm.jar.origin ] && mv target/hola-swarm.jar target/hola-swarm.jar.origin
 
 cd target/hola-swarm.jar.dir
